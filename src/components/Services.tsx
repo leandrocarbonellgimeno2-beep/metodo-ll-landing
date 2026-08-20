026fc6298f4efb91a7b390b02e9f2336c53097ab
Leandro Carbonell <leandrocarbonellgimeno2@gmail.com>
2026-08-20 12:53:53 +0200

    fix: update Nivel 4 subprice to (o \ USD)

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -62,7 +62,7 @@ const services = [
       "Acompañamiento directo de la mano de Lucas",
     ],
     price: "$980.000 ARS",
-    subprice: "o 800 USD",
+    subprice: "(o $800 USD)",
     buttonText: "Aplicar Ahora",
     waMessage: "Hola Lucas! Me interesa aprender a invertir en negocios rentables.",
   },
